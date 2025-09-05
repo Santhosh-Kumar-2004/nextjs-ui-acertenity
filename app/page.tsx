96e798e5ea0be18888f1cb88630b462d7232a071
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-05 12:29:56 +0530

    Added wavy bg styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { BackgroundGradientDemo } from "@/components/bgGradient";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { BackgroundGradientAnimationDemo } from "@/components/gradient_bg";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import { WavyBackgroundDemo } from "@/components/wavy_bg_demo";
 
 export default function Home() {
   return (
     <>
-      <SparklesCore />  
-      <SparklesPreview />
+      {/* <SparklesCore />  
+      <SparklesPreview /> */}
       {/* <BackgroundGradient /> */}
       {/* <BackgroundGradientDemo /> */}
-      <BackgroundGradientAnimation />
-      <BackgroundGradientAnimationDemo />
+      {/* <BackgroundGradientAnimation />
+      <BackgroundGradientAnimationDemo /> */}
+      <WavyBackground />
+      <WavyBackgroundDemo />
     </>
   );
 }
